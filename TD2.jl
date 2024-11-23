0520f3fe25ecb03bee221958186e8c060cf94979
Paul Fraenkel <paul.fraenkel@etu.minesparis.psl.eu>
2024-11-23 22:03:34 +0100

    debut partie 2 TD 2

--- TD2.jl
+++ TD2.jl
@@ -71,11 +71,78 @@ mutable struct LinkedList{T}
 end
 
 function LinkedList{T}(l::Vector{T}) where {T}
+    if Base.length(l) == 0 return LinkedList{T}(); end;
     list = LinkedList{T}()
-    for elem in l 
-        list.element = elem
-        newList = LinkedList{T}()
-        newList.next
+    elem = popfirst!(l)
+    list.element = elem
+    list.next = LinkedList{T}(l)
+    return list
+end
+
+function length(ll::LinkedList)
+    len = 0
+    list = ll
+    while ~(list.next === list)
+        len += 1
+        list = list.next
     end
-    list 
+    return len
+end
+
+
+function minimum(ll::LinkedList)
+    if ll.next === ll
+        return Inf
+    else
+        return min(ll.element, minimum(ll.next))
+    end
+end
+
+#Les fonctions minimum et length sont de complexité linéaire par rapport à n
+# Je n'arrive malheureusement pas à trouver la complexité des fonctions de base de julia...
+
+# Exercice 4
+mutable struct SimpleGraph
+    neighbors::Array{Vector{Int}, 1}
+    n_sommets::Int
+    n_aretes::Int
+
+    function SimpleGraph()
+        graph = new(Array{Vector{Int}}[])
+        graph.n_sommets = 0
+        graph.n_aretes = 0
+        return graph
+    end
+end
+
+function nv(sg::SimpleGraph)
+    return sg.n_sommets
+end
+
+function ne(sg::SimpleGraph)
+    return sg.n_aretes
+end
+
+function add_vertices!(sg::SimpleGraph, n)
+    sg.n_sommets += n
+    for i in 1:n 
+        push!(sg.neighbors, Vector{Int64}[])
+    end
+end
+
+function add_edge!(sg::SimpleGraph, edge::Tuple{Int, Int})
+    maximum(edge) <= nv(sg) || return false
+    if edge[2] in sg.neighbors[edge[1]] return false; end
+    push!(sg.neighbors[edge[1]], edge[2])
+    sg.n_aretes += 1
+    return true
+end
+
+function exemple_graph()
+    sg = SimpleGraph()
+    add_vertices!(sg, 8)
+    for edge in [(1, 4), (2, 1), (3, 2), (3, 8), (4, 1), (4, 3), (4, 6), (5, 7), (8, 6)]
+        add_edge!(sg, edge)
+    end
+    sg
 end
