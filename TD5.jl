4e28dbb93e01b52a5cd4d3cf9e53712542e40725
Paul Fraenkel <paul.fraenkel@etu.minesparis.psl.eu>
2024-12-31 16:56:08 +0100

    milieu exo 4 TD5

--- TD5.jl
+++ TD5.jl
@@ -15,15 +15,24 @@ function subtrees(tree::Tree)
     tree.branches
 end
 
-function children(tree::AbstractTree{T})
-    [root(t) for t in subtrees(t)]
+function children(tree::AbstractTree)
+    [root(t) for t in subtrees(tree)]
 end
 
-function Base.iterate(tree::AbstractTree{T}, chain)
-    trees, i = chain
+function Base.iterate(tree::AbstractTree, chain=-1)
+    #initialisation
+    if chain == -1
+        trees = [tree,]
+        i = 1;
+    else
+        #trees permet de stocker tous les sous-arbres du niveau de profondeur suivant
+        trees, i = chain
+    end
+
     i <= length(trees) && return (root(trees[i]), (trees, i+1))
 
-    subtree = AbstractTree{T}[]
+    #Dans ce cas, on a exporé tous les sous-arbres de niveau, il faut passer au niveau suivant.
+    subtree = AbstractTree[]
     for t in trees
         length(subtrees(t)) > 0 && append!(subtree, subtrees(t))
     end
@@ -33,7 +42,7 @@ function Base.iterate(tree::AbstractTree{T}, chain)
     return nothing
 end
 
-# Exercice 6 : 
+# Exercice 2 : 
 
 # Un arbre binaire est équivalent à une liste chaînée si chaque noeud possède exactement un sous-noeud
 
@@ -46,7 +55,137 @@ struct BinaryTree{T} <: AbstractTree{T}
     end
 end
 
-#=function BinaryTree(l::Vector)
-    T=eltype(l)
-    tree = BinaryTree{T}(l[1], ())
-end=#
+function root(tree::BinaryTree)
+    tree.element
+end
+
+function subtrees(tree::BinaryTree)
+    tree.branches
+end
+
+function BinaryTree{T}(l::Vector) where {T}
+    L = length(l)
+
+    #J'utilise ici un dictionnaire, mais il aurait été possible de simplement utiliser
+    # une liste car les éléments auxquels on va accéder sont ordonnés
+    dict = Dict()
+
+    for (i, elem) in Iterators.reverse(enumerate(l))
+        if L >= 2*i+1
+            subTree = BinaryTree{T}(elem, [dict[2*i], dict[2*i+1]])
+        elseif L == 2*i #L >= 2*i
+            subTree = BinaryTree{T}(elem, [dict[2*i]])
+        else
+            subTree = BinaryTree{T}(elem, BinaryTree{T}[])
+        end
+        push!(dict, i => subTree)
+    end
+
+    return dict[1]
+end
+
+#> BinaryTree([i for i in 1:4])
+#BinaryTree{Int64}(1, BinaryTree{Int64}[BinaryTree{Int64}(2, BinaryTree{Int64}[BinaryTree{Int64}(4, BinaryTree{Int64}[])]), BinaryTree{Int64}(3, BinaryTree{Int64}[])])
+
+#> BinaryTree([i for i in 1:5])
+#BinaryTree{Int64}(1, BinaryTree{Int64}[BinaryTree{Int64}(2, BinaryTree{Int64}[BinaryTree{Int64}(4, BinaryTree{Int64}[]), BinaryTree{Int64}(5, BinaryTree{Int64}[])]), BinaryTree{Int64}(3, BinaryTree{Int64}[])])
+
+# Un arbre généré de cette manière a toutes ses feuillles à la même profondeur
+# s'il existe n tel que length(l) = n^2 - 1
+
+
+# Nécessaire pour utiliser collect()
+function Base.length(tree::BinaryTree)
+    subTrees = subtrees(tree)
+    sum = 1
+
+    for subtree in subTrees
+        sum += length(subtree)
+    end
+
+    return sum
+end
+
+#Exercice 4
+
+function isheap(tree::BinaryTree)
+    #On ne vérifie pas ici le caractère complet de l'arbre
+
+    subTrees = subtrees(tree)
+    length(subTrees) == 0 && return true
+
+    for subtree in subTrees
+        isheap(subtree) || return false
+        root(subtree) > root(tree) || return false
+    end
+
+    return true
+end
+
+struct Heap{T} <: AbstractTree{T}
+    elements::Vector{T}
+end
+
+function root(heap::Heap)
+    return heap.elements[0]
+end
+
+function isInBranch(i, n)
+    i == 1 && return false
+    exponent = trunc(Int, log2(i))
+    base = 2^exponent
+
+    ((i % base) < (base/2)) ⊻ (n == 2)
+end
+
+function subtrees(heap::Heap)
+    subtree1 = [x for (i, x) in enumerate(heap.elements) if isInBranch(i, 1)]
+    subtree2 = [x for (i, x) in enumerate(heap.elements) if isInBranch(i, 2)]
+    T = eltype(heap.elements)
+
+    return [Heap{T}(subtree1), Heap{T}(subtree2)]
+end
+
+function Heap{T}(tree::BinaryTree) where {T}
+    queue = [tree]
+    elements = []
+
+    while length(queue) > 0
+        current = popfirst!(queue)
+        push!(elements, root(current))
+
+        for child in subtrees(current)
+            push!(queue, child)
+        end
+    end
+
+    return Heap{T}(elements)
+end
+
+function BinaryTree{T}(heap::Heap) where {T}
+    return BinaryTree{T}(heap.elements)
+end
+
+function Base.push!(h::Heap{T}, x::T) where {T}
+    push!(h.elements, x)
+
+    current = length(h.elements)
+    while (h.elements[current] < h.elements[current ÷ 2]) && (current != 1)
+        h.elements[current], h.elements[current ÷ 2] = h.elements[current ÷ 2], h.elements[current]
+        current ÷= 2
+    end
+end
+
+function Base.pop!(h::Heap)
+    root_ = root(h)
+    h.elements[1] = pop!(h.elements)
+
+    current = 1
+    while (h.elements[current] < h.elements[2 * current]) || (h.elements[current] < h.elements[2 * current + 1])
+        if 
+            
+        else
+
+        end
+    end
+end
---
+++ TD5_test.jl
@@ -0,0 +1,144 @@
+include("TD5.jl")
+
+#Exercice 1
+
+function testTree() 
+    subTree1 = Tree{Int}(2, [Tree{Int}(i, Vector{Tree{Int}}()) for i in 4:6])
+    subTree2 = Tree{Int}(3, [Tree{Int}(i, Vector{Tree{Int}}()) for i in 7:8])
+    return Tree{Int}(1, Tree{Int}[subTree1, subTree2])
+end
+
+function test_iterate()
+    test_tree = testTree()
+    for tree in test_tree
+        println(tree)
+    end
+end
+
+#Exercice 2
+
+function testBinaryTree() 
+    subTree1 = BinaryTree{Int}(2, [BinaryTree{Int}(i, Vector{BinaryTree{Int}}()) for i in 4:6])
+    subTree = BinaryTree{Int}(3, [BinaryTree{Int}(i, Vector{BinaryTree{Int}}()) for i in 7:8])
+    return BinaryTree{Int}(1, BinaryTree{Int}[subTree1, subTree2])
+end
+
+function Base.size(tree::BinaryTree)
+    subTrees = subtrees(tree)
+    length(subTrees) == 0 && return 1
+
+    sum = 0
+    for subtree in subTrees
+        sum += size(subtree)
+    end
+
+    return sum
+end
+
+function append_print!(tree::BinaryTree, lines::Vector{String}, depth::Int)
+    if length(lines) < depth
+        if length(lines) == 0# || lines[1][end-1] != ' '
+            offset = 0
+        else
+            offset = length(lines[1]) - 2
+        end
+        #offset = length(lines) == 0 ? 0 : length(lines[end])
+        push!(lines, repeat(" ", offset))
+    end
+
+    subTrees = subtrees(tree)
+
+    #=println()
+    println("new")
+    for l in lines
+        println(l)
+    end=#
+
+    if length(subTrees) == 0
+        for i in 1:length(lines)
+            length(lines[i]) > length(lines[depth]) && continue
+            lines[i] *= i == depth ? string(root(tree)) : " "
+            lines[i] *= " "
+        end
+    else
+        lines[depth] *= string(root(tree)) * " "
+        for subtree in subTrees
+            append_print!(subtree, lines, depth+1)
+        end
+    end
+end
+
+function print(tree::BinaryTree)
+    #=lines = String[]
+
+    # place all the values in the table
+    append_print!(tree, lines, 1)
+
+    # find the number of inter-lines
+    ninter = fill(1, length(lines))
+    for i in 2:length(lines)
+        l = lines[i]
+
+        for j in 1:2:length(l)
+            if ~(l[j] == ' ')
+                # find parent
+
+                k = 0
+                while lines[i-1][j - 2*k] == ' '
+                    k += 1
+                end
+                ninter[i] = max(ninter[i], k)
+            end
+        end
+    end
+    
+    for (i, l) in enumerate(lines)
+        i == 1 && begin; println(l); continue; end
+
+        for j in 1:ninter[i]
+            println("f");
+        end
+
+        println(l)
+    end
+
+    println()
+    # final display
+    for l in lines
+        println(l)
+    end=#
+
+    # 1
+    # |\
+    # | \
+    # |  \
+    # |   \
+    # 2    3
+    # |\   |\
+    # | \  | |
+    # 4  5 6 7
+    # |\
+    # 8 9
+
+    #         1
+    #        / \
+    #       /   \
+    #      /     \
+    #     2       3
+    #    / \     / \
+    #   4   5   6   7
+    #  / \
+    # 8   9
+
+    # 1
+    # |-2
+    # | |-4
+    # | | |-8
+    # | | |-9
+    # | | 
+    # | |-5
+    # |
+    # |-3
+    #   |-6
+    #   |-7
+end
